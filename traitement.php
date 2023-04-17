257cf7a849d4fd51f18d171406f79aa312058b02
Abdarrahmane-NEINE <83295620+Abdarrahmane-NEINE@users.noreply.github.com>
2023-04-17 11:18:38 +0000

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,26 +1,34 @@
 <?php
     require "connect.php";
 
+try{
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->beginTransaction();
 
-    //préparation de la requet
-    $pdoStatement = $pdo->prepare('INSERT INTO gse_d_notation(IdNotification, Notation, Commentaire, DateCrea, IdentificationUser) VALUES (NULL, :Notification, :Commentaire, NULL, :IdUser)');
+  //préparation de la requet
+  $pdoStatement = $pdo->prepare('INSERT INTO notation (IdNotification, Notation, Commentaire, DateCrea, IdentificationUser) VALUES (NULL, :Notification, :Commentaire, NULL, :IdUser)');
 
-    //liaison entre les parametres et leur valeur
-    $pdoStatement->bindValue(':Notification', $_POST['Notification']);
-    $pdoStatement->bindValue(':Commentaire', $_POST['comment'], PDO::PARAM_STR);
-    $pdoStatement->bindValue(':IdUser', $_POST['user'], PDO::PARAM_STR);
+  //liaison entre les parametres et leur valeur
+  $pdoStatement->bindValue(':Notification', $_POST['Notification']);
+  $pdoStatement->bindValue(':Commentaire', $_POST['comment'], PDO::PARAM_STR);
+  $pdoStatement->bindValue(':IdUser', $_POST['user'], PDO::PARAM_STR);
 
 
-    //éxécution de la requete préparée
+  //éxécution de la requete préparée
 
-    $insertion = $pdoStatement->execute();
+  $insertion = $pdoStatement->execute();
 
-    if($insertion){
-      // $message = 'La notification a été ajoutée dans la bdd';
-      echo http_response_code(201);
-    }
-    else {
-        echo http_response_code(501);
-    }
+  if($insertion){
+    $message = 'La notification a été ajoutée dans la bdd';
+    http_response_code(201);
+    echo $message;
+  }
 
- ?>
+  $pdo->commit();
+
+}catch(Exception $e){
+    // echo 500;
+    // for debug
+    echo '{"code":"500","message":"Erreur","code_mysql":"'.$e->getCode().'","message_mysql" : "'.$e->getMessage().'" }';
+
+}
